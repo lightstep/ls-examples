77b59d31cec77aae33f0a9bcf81824803db89144
Matthew Wear <mwear@lightstep.com>
2020-03-20 13:49:35 -0700

    Do not include scheme in LIGHTSTEP_HOST

--- js/client/client.js
+++ js/client/client.js
@@ -4,9 +4,9 @@ const LIGHTSTEP_ACCESS_TOKEN = process.env.LIGHTSTEP_ACCESS_TOKEN;
 const COMPONENT_NAME =
   process.env.LIGHTSTEP_COMPONENT_NAME || 'ls-trace-js-client';
 const TARGET_URL = process.env.TARGET_URL || 'http://localhost:8080/ping';
-const LIGHTSTEP_HOST =
-  process.env.LIGHTSTEP_HOST || 'https://ingest.lightstep.com';
-const LIGHTSTEP_METRICS_URL = `${LIGHTSTEP_HOST}/metrics`;
+const LIGHTSTEP_HOST = process.env.LIGHTSTEP_HOST || 'ingest.lightstep.com';
+const LIGHTSTEP_URL = `https://${LIGHTSTEP_HOST}`;
+const LIGHTSTEP_METRICS_URL = `${LIGHTSTEP_URL}/metrics`;
 
 const tracer = require('ls-trace').init({
   experimental: {
@@ -16,7 +16,7 @@ const tracer = require('ls-trace').init({
   runtimeMetrics: true,
   reportingInterval: 30 * 1000,
   componentName: COMPONENT_NAME,
-  url: LIGHTSTEP_HOST,
+  url: LIGHTSTEP_URL,
   metricsUrl: LIGHTSTEP_METRICS_URL,
   tags: `lightstep.service_name:${COMPONENT_NAME},lightstep.access_token:${LIGHTSTEP_ACCESS_TOKEN}`,
 });
--- js/server/server.js
+++ js/server/server.js
@@ -4,9 +4,9 @@ const LIGHTSTEP_ACCESS_TOKEN = process.env.LIGHTSTEP_ACCESS_TOKEN;
 const COMPONENT_NAME =
   process.env.LIGHTSTEP_COMPONENT_NAME || 'ls-trace-js-server';
 const PORT = process.env.PORT || 8080;
-const LIGHTSTEP_HOST =
-  process.env.LIGHTSTEP_HOST || 'https://ingest.lightstep.com';
-const LIGHTSTEP_METRICS_URL = `${LIGHTSTEP_HOST}/metrics`;
+const LIGHTSTEP_HOST = process.env.LIGHTSTEP_HOST || 'ingest.lightstep.com';
+const LIGHTSTEP_URL = `https://${LIGHTSTEP_HOST}`;
+const LIGHTSTEP_METRICS_URL = `${LIGHTSTEP_URL}/metrics`;
 
 const express = require('express');
 const tracer = require('ls-trace').init({
@@ -17,7 +17,7 @@ const tracer = require('ls-trace').init({
   runtimeMetrics: true,
   reportingInterval: 30 * 1000,
   componentName: COMPONENT_NAME,
-  url: LIGHTSTEP_HOST,
+  url: LIGHTSTEP_URL,
   metricsUrl: LIGHTSTEP_METRICS_URL,
   tags: `lightstep.service_name:${COMPONENT_NAME},lightstep.access_token:${LIGHTSTEP_ACCESS_TOKEN}`,
 });
