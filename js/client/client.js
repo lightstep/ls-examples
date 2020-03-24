6b2c904602778848150b1485c901ea7ae7f8dbb1
Matthew Wear <mwear@lightstep.com>
2020-03-24 14:25:38 -0700

    Update js example with new config / latest source

--- js/client/client.js
+++ js/client/client.js
@@ -1,24 +1,16 @@
 'use strict';
 
-const LIGHTSTEP_ACCESS_TOKEN = process.env.LIGHTSTEP_ACCESS_TOKEN;
+const ACCESS_TOKEN = process.env.SECRET_ACCESS_TOKEN;
 const COMPONENT_NAME =
   process.env.LIGHTSTEP_COMPONENT_NAME || 'ls-trace-js-client';
+const SERVICE_VERSION = process.env.LIGHTSTEP_SERVICE_VERSION || '0.0.1';
 const TARGET_URL = process.env.TARGET_URL || 'http://localhost:8080/ping';
-const LIGHTSTEP_HOST = process.env.LIGHTSTEP_HOST || 'ingest.lightstep.com';
-const LIGHTSTEP_URL = `https://${LIGHTSTEP_HOST}`;
-const LIGHTSTEP_METRICS_URL = `${LIGHTSTEP_URL}/metrics`;
 
 const tracer = require('ls-trace').init({
   experimental: {
     b3: true,
   },
-  clientToken: LIGHTSTEP_ACCESS_TOKEN,
-  runtimeMetrics: true,
-  reportingInterval: 30 * 1000,
-  componentName: COMPONENT_NAME,
-  url: LIGHTSTEP_URL,
-  metricsUrl: LIGHTSTEP_METRICS_URL,
-  tags: `lightstep.service_name:${COMPONENT_NAME},lightstep.access_token:${LIGHTSTEP_ACCESS_TOKEN}`,
+  tags: `lightstep.service_name:${COMPONENT_NAME},lightstep.access_token:${ACCESS_TOKEN},service.version:${SERVICE_VERSION}`,
 });
 const scope = tracer.scope();
 
