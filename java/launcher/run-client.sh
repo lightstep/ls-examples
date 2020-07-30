da19f9e0c0f591bc5e04782ee5210edaf78d3664
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2020-07-30 18:26:30 +0200

    Example for testing the Launcher.

---
+++ java/launcher/run-client.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export LS_SERVICE_NAME="java-client"
+export LS_SERVICE_VERSION="0.0.1"
+export LS_ACCESS_TOKEN=""
+
+#export OTEL_EXPORTER_OTLP_SPAN_ENDPOINT="staging.ingest.lightstep.com"
+#export OTEL_EXPORTER_OTLP_SPAN_INSECURE="true"
+#export OTEL_PROPAGATORS="tracecontext"
+#export OTEL_LOG_LEVEL=""
+#export OTEL_RESOURCE_LABELS="host=here.com,deployment=99"
+
+java -javaagent:lightstep-opentelemetry-javaagent-0.1.0-SNAPSHOT.jar \
+	-jar target/client.jar
